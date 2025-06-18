5b4c5613f1c26a5e7a891a209529cf3b63c09f30
salsa-bela <mockingjaystud@gmail.com>
2025-06-18 22:01:39 +0700

    upgrade controller, icon

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,12 +8,6 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $schoolInfo = [
-            'name' => 'Gema Music School Yogyakarta',
-            'slogan' => 'From keys to strings, let your passion sings',
-            'desc' => 'Gema Music School adalah harmoni dari mimpi dan melodi, tempat di mana setiap nada menjadi jembatan menuju ekspresi diri yang mendalam. Di sinilah suara hati menemukan iramanya, dan jemari kecil belajar menari di atas tuts dan senar. Lebih dari sekadar sekolah musik, Gema adalah ruang kreatif penuh semangat, di mana bakat diasah, passion dipupuk, dan keberanian untuk tampil tumbuh seiring waktu.'
-        ];
-
-        return view('home', compact('schoolInfo'));
+        return view('home'); // akan memanggil resources/views/home.blade.php
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/beranda', function () {
-    return view('beranda');
-});
-
 use App\Http\Controllers\HomeController;
+
 Route::get('/', [HomeController::class, 'index']);
