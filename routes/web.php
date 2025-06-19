907eff908f77c133d09a29c943a7dc04a431e626
Ovan1616 <ovantamvan62191@gmail.com>
2025-06-19 17:38:52 +0700

    Menambahkan controller untuk jadwal dan profil

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JadwalController;
+use App\Http\Controllers\ProfilController;
 
+// Halaman utama (tetap)
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/jadwal', function () {
-    return view('jadwal');
-});
-Route::get('/profil', function () {
-    return view('profil');
-});
 
+// GUNAKAN CONTROLLER untuk jadwal dan profil
+Route::get('/jadwal', [JadwalController::class, 'index']);
+Route::get('/profil', [ProfilController::class, 'index']);
+Route::get('/profil/edit', [ProfilController::class, 'edit']); // opsional edit profil
